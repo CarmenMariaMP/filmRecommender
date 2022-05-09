f2c1b88200fa3834d37696d530cc1fb9839e9920
Francisco Rodríguez Pérez <56715417+Fco-bit@users.noreply.github.com>
2022-05-09 19:43:28 +0200

    Update FilmsRecommenderService.java

--- src/main/java/com/filmRecommender/Service/FilmsRecommenderService.java
+++ src/main/java/com/filmRecommender/Service/FilmsRecommenderService.java
@@ -16,20 +16,20 @@ public class FilmsRecommenderService {
 	FilmRepository filmRepository;
 
     public  void  runExample(){
-        System.out.println("Peliculas con el director: ");
+        System.out.println("Peliculas con el director: Clive_Donner");
         Collection<Pelicula> filmsDirector = filmRepository.findFilmsByDirector("Clive_Donner");
-        System.out.println(filmsDirector);
+        filmsDirector.forEach(film -> System.out.println(film.getNombre()));
+        System.out.println("Peliculas con el guionista: Rich Eustis");
         Collection<Pelicula> filmsGuionista = filmRepository.findFilmsByGuionista("Rich Eustis");
-        System.out.println(filmsGuionista);
+        filmsGuionista.forEach(film -> System.out.println(film.getNombre()));
+        System.out.println("Peliculas con el compositor: Nagesh Kukunoor");
         Collection<Pelicula> filmsCompositor = filmRepository.findFilmsByCompositor("Nagesh Kukunoor");
-        System.out.println(filmsCompositor);
+        filmsCompositor.forEach(film -> System.out.println(film.getNombre()));
+        System.out.println("Peliculas del pais: France");
         Collection<Pelicula> filmsPais = filmRepository.findFilmsByPais("France");
-        System.out.println(filmsPais);
+        filmsPais.forEach(film -> System.out.println(film.getNombre()));
+        System.out.println("Peliculas con el actor: Jack_Klaff");
         Collection<Pelicula> filmsActor = filmRepository.findFilmsByActor("Jack_Klaff");
-        System.out.println(filmsActor);
-        
-
-
+        filmsActor.forEach(film -> System.out.println(film.getNombre()));
     }
-    
 }
